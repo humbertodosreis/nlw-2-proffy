fb6277c21690695cb543778f29662fdca1f6d870
Humberto dos Reis Rodrigues <rrodrigues.humberto@gmail.com>
2020-08-09 02:32:52 -0300

    adding server

---
+++ server/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    parserOptions: {
+        ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+    },
+    plugins: ['@typescript-eslint', 'no-loops', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    rules: {
+        'no-console': 1,
+        'no-loops/no-loops': 1,
+        'prettier/prettier': 2,
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    },
+};
